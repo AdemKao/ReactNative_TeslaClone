b5501115ffdab82400812f994de97624a5652af4
AdemKao <adem60513@gmail.com>
2022-06-18 19:22:48 +0800

    botton component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
-import styles from "./sytle";
+import StyleButton from "../StyleButton";
+import styles from "./styles";
 
 const CarItem = (props) => {
   return (
@@ -10,6 +11,20 @@ const CarItem = (props) => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+      <StyleButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Custom Order was pressed");
+        }}
+      />
+      <StyleButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("Existing Inventory was pressed");
+        }}
+      />
     </View>
   );
 };
